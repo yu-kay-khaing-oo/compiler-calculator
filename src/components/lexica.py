abaedc9d9d2ca07eb7312fcf73f11f3ec7233ed7
yu-kay-khaing-oo <yukaykhaingoo@my.jcu.edu.au>
2024-03-31 21:07:30 +0630

    Commit changes

--- src/components/lexica.py
+++ src/components/lexica.py
@@ -1,5 +1,4 @@
 from sly import Lexer
-import sly
 
 class MyLexer(Lexer):
     """
@@ -11,41 +10,20 @@ class MyLexer(Lexer):
     Python regEX: https://www.w3schools.com/python/python_regex.asp
     """
 
-    ### `tokens` ###
-    # set `tokens` so it can be used in the parser.
-    # This must be here and all Capitalized. 
-    # Please, ignore IDE warning.
-    tokens = { ASSIGN, NAME, NUMBER, MINUS, DIVIDE, TIMES, LPAREN, RPAREN}
-    
-    # https://sly.readthedocs.io/en/latest/sly.html#literal-characters
-    literals = { '+' }
-    
-    ### matching rule ###
-    # The matching work from top to bottom
-    # At least, all toekns must be defined here
+    # Set of token names
+    tokens = { NUMBER, PLUS, MINUS, DIVIDE, TIMES, LPAREN, RPAREN}
 
-    # Ignore spaces and tabs 
-    ignore = ' \t'
-
-    ### EX1: simply define with regEX ###
-    NAME = r'[a-zA-Z_][a-zA-Z0-9_]*'
-    ### EX2: Define as a function ###
-    @_(r'\d+')
-    def NUMBER(self, token):
-        # Note that this function set parse token.value to integer
-        token.value = int(token.value)
-        # Extra print for debug
-        print(f"====This print from NUMBER function: {token.type=} {token.value=} {type(token.value)=}")
-        return token
-
-    # Try uncomment this and run to see the differences between `token` and `literal`
-    ASSIGN  = r'\='
-    # PLUS    = r'\+'
+    # Regular expression rules for tokens
+    NUMBER  = r'\d+'
+    PLUS    = r'\+'
     MINUS   = r'-'
     TIMES   = r'\*'
     DIVIDE  = r'/'
     LPAREN  = r'\('
     RPAREN  = r'\)'
+
+    # Ignore spaces and tabs 
+    ignore = ' \t'
 
     # Extra action for newlines
     @_(r'\n+')
@@ -56,12 +34,3 @@ class MyLexer(Lexer):
     def error(self, t):
         self.index += 1
         print(f"ERROR: Illegal character '{t.value[0]}' at line {self.lineno}")
-
-if __name__ == '__main__':
-    # Write a simple test that only run when you execute this file
-    string_input:str = "x1 + 1as! * ()"
-    lex:Lexer = MyLexer()
-    # assign type to `token`
-    token: sly.lex.Token
-    for token in lex.tokenize(string_input):
-        print(token)
--- src/main.py
+++ src/main.py
@@ -1,54 +1,66 @@
 import sys
 from PyQt6 import uic
 from PyQt6.QtWidgets import QApplication
-from PyQt6.QtWidgets import QMainWindow, QLineEdit, QPushButton, QLCDNumber
+from PyQt6.QtWidgets import QMainWindow, QLineEdit, QPushButton, QLCDNumber, QLabel
 
 from components.lexica import MyLexer
 from components.parsers import MyParser
-from components.memory import Memory
 
 class MainWindow(QMainWindow):
 
     # Do this for intellisense
+    button_0:QPushButton
     button_1:QPushButton
     button_2:QPushButton
+    button_3:QPushButton
+    button_4:QPushButton
+    button_5:QPushButton
+    button_6:QPushButton
+    button_7:QPushButton
+    button_8:QPushButton
+    button_9:QPushButton
     button_plus:QPushButton
+    button_multiply:QPushButton
     button_equal:QPushButton
     input_text:QLineEdit
     output_lcd:QLCDNumber
+    pre_notation:QLabel
+    post_notation:QLabel
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         uic.loadUi("components/main.ui", self)
 
         #### Binding button to function ####
-        # Method 1:
-        self.button_1.clicked.connect(self.push_1)
-        # Method 2:
+        self.button_0.clicked.connect(lambda: self.push("0"))
+        self.button_1.clicked.connect(lambda: self.push("1"))
         self.button_2.clicked.connect(lambda: self.push("2"))
+        self.button_3.clicked.connect(lambda: self.push("3"))
+        self.button_4.clicked.connect(lambda: self.push("4"))
+        self.button_5.clicked.connect(lambda: self.push("5"))
+        self.button_6.clicked.connect(lambda: self.push("6"))
+        self.button_7.clicked.connect(lambda: self.push("7"))
+        self.button_8.clicked.connect(lambda: self.push("8"))
+        self.button_9.clicked.connect(lambda: self.push("9"))
+        
         self.button_plus.clicked.connect(lambda: self.push("+"))
+        self.button_multiply.clicked.connect(lambda: self.push("*"))
 
         self.button_equal.clicked.connect(self.push_equal)
 
-    def push_1(self):
-        current_text:str = self.input_text.text()
-        self.input_text.setText(f"{current_text}1")
-    
     def push(self, text:str):
         current_text:str = self.input_text.text()
         self.input_text.setText(f"{current_text}{text}")
     
     def push_equal(self):
-        print("Calculate")
         lexer = MyLexer()
         parser = MyParser()
-        memory = Memory()
         input_text = self.input_text.text()
-        result = parser.parse(lexer.tokenize(input_text))
-        print(type(result))
+        if not input_text:
+            result = 0
+        else:
+            result = parser.parse(lexer.tokenize(input_text))
         self.output_lcd.display(result)
-        # for debug
-        print(memory)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
